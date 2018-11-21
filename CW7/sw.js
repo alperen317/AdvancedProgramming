5efb228f76cf13a84469034dfb99e46cb8efe55b
Ali Alperen ASLAN <alialperen.aslan@stu.fsm.edu.tr>
2018-11-21 18:20:36 +0300

    sw.js

--- CW7/sw.js
+++ CW7/sw.js
@@ -1,72 +1,23 @@
-//Version
+const CACHE ='JS'
+function installCB(e) {
+  console.log('install oldu', e.request);
+}
+self.addEventListener('install', installCB)
 
-var appVersion = 'v1.00'
-
-
-
-//Fetch to cache
-var files=[
-
-  './',
-  './index.html',
-  './cw7.html'
-
-
-
-]
-
-
-
-//Install
-self.addEventListener('install',event=> {
-	
-	event.waitUntil(
-	   caches.open(appVersion)
-	   .then(cache=>{
-		   return cache.addAll(files)
-		   .catch(err=>{
-			   console.err('Error adding files to cache',err)
-			   
-		   })
-		   
-	   })
-	)
-	console.info('SW Installed')
-	self.skipWaiting();
-})
-
-
-//Active
-self.addEventListener('active',event=>{
-	event.waitUntil(
-		caches.keys()
-		.then(cacheNames => {
-			return Promise.all(
-			   cacheNames.map(cache => {
-				   if(cache !== appVersion){
-					   console.info('Deleting Old Cache',cache)
-					   return caches.delete(cache);
-				   }
-			   })
-			)
-		})
-	)
-	return self.clients.claim();
-})
-
-
-
-//self
-self.addEventListener('fetch', event=> {
-	console.info('SW fetch',event.request.url);
-	event.respondWith(
-	 caches.match(event.request)
-	 .then(res =>{
-		 return res || fetch(event.request)   
-	 })
-	)
-})
-
-
-
-//Fetch
+function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  })
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  console.log('JS', req.url);
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
+  )
+}
+self.addEventListener('fetch', fetchCB)
